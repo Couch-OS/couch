f943d4063efd95b50cb1141a513bc4d6c87b857a
Bryan Hoban <bryan.hoban@gmail.com>
2026-09-23 13:56:35 -0400

    Prepare protocol 3 compatibility renewal

--- daemon/couch-confd/src/main.rs
+++ daemon/couch-confd/src/main.rs
@@ -48,6 +48,7 @@ fn main() {
     let early: Vec<String> = std::env::args().skip(1).collect();
     if early == ["--supports-integration-protocol=1"]
         || early == ["--supports-integration-protocol=2"]
+        || early == ["--supports-integration-protocol=3"]
     {
         return;
     }
